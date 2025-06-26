8c7d96ca45eca007cd76a4a3a435ab8005c08693
Rob Falck <rfalck@nasa.gov>
2025-06-26 11:06:49 -0400

    simplified create_xdsm

--- openmdao/visualization/xdsm_viewer/xdsm_viewer.py
+++ openmdao/visualization/xdsm_viewer/xdsm_viewer.py
@@ -1,80 +1,15 @@
-from collections import deque
 
 import openmdao.api as om
 
-# from openmdao.core.system import System
+from typing import Sequence
+
 from openmdao.core.component import Component
 from openmdao.core.implicitcomponent import ImplicitComponent
-# from openmdao.solvers.solver import Solver
-# from openmdao.core.driver import Driver
 
 try:
-    from pyxdsm.XDSM import XDSM, SOLVER, FUNC, GROUP
+    from pyxdsm.XDSM import XDSM, SOLVER, OPT, FUNC, GROUP
 except ImportError:
     XDSM = None
-
-
-# def _all_subsys_iter(sys, include_self=False, recurse=True, typ=None,
-#                      recurse_exceptions=None, solvers_active=True,
-#                      include_nl_run_once=False):
-#     """
-#     Yield a generator of local subsystems of this system.
-
-#     Parameters
-#     ----------
-#     include_self : bool
-#         If True, include this system in the iteration.
-#     recurse : bool
-#         If True, iterate over the whole tree under this system.
-#     typ : type
-#         If not None, only yield Systems that match that are instances of the
-#         given type.
-#     recurse_exceptions : set
-#         A set of system pathnames that should not be recursed, if recurse is True.
-#     solvers_active : bool
-#         If True, solvers are active and should be included in the hierarchy
-#         as we proceed down through the model. Otherwise, they are ignored.
-#         When a NewtonSolver with solve_subsystems=False is encountered, this
-#         option is set to false so that the XDSM conveys that the inner solvers are not active.
-
-#     Yields
-#     ------
-#     type or None
-#     """
-#     from openmdao.core.group import Group
-#     from openmdao.solvers.nonlinear.nonlinear_runonce import NonlinearRunOnce
-
-#     _recurse_excptions = recurse_exceptions or []
-
-#     if include_self and (typ is None or isinstance(sys, typ)):
-#         print(f'yielding {sys} line 50')
-#         yield sys
-
-#     if isinstance(sys, Group):
-#         if recurse:
-#             ignore_solver = False
-#             if not include_nl_run_once and isinstance(sys.nonlinear_solver, NonlinearRunOnce):
-#                 ignore_solver = True
-#             if isinstance(sys.nonlinear_solver, om.NewtonSolver) and not sys.nonlinear_solver.options['solve_subsystems']:
-#                 solvers_active = False
-#                 # If Newton is encountered without solve subsystems, yield this solver but no inner solvers for the diagram.
-#             if not ignore_solver:
-#                 yield sys.nonlinear_solver
-#         else:
-#             print(f'yielding {sys} line 64')
-#             yield sys
-
-#     for subsys_name in sys._subsystems_allprocs:
-#         subsys = sys._get_subsystem(subsys_name)
-#         if typ is None or isinstance(subsys, typ):
-#             print(f'yielding {sys} line 70')
-#             yield subsys
-
-#         if recurse and subsys.pathname not in _recurse_excptions:
-#             for sub in _all_subsys_iter(subsys, include_self=False, recurse=True, typ=typ,
-#                                         recurse_exceptions=recurse_exceptions):
-#                 print(f'yielding {sub} line 50')
-#                 yield sub
 
 
 def _make_legal_node_name(s):
@@ -95,21 +30,49 @@ def _make_legal_node_name(s):
         s = s.replace(_char, '_')
     return s
 
-def _detokenize(s):
-    r"""
-    Wrap the given string in the LaTeX \detokenize command.
 
-    Parameters
-    ----------
-    s : str
-        The string to be detokenized.
+def _make_label(label, font='textsc'):
+    if isinstance(label, str):
+        label = [label]
+    ret = []
+    for i, s in enumerate(label):
+        # ds = _detokenize(s)
+        # ret.append(rf'\textnormal{{{ds}}}')
+        ret.append(_format_label_line(s, font=font))
+    return ret
 
-    Returns
-    -------
-    str
-        The string wrapped in \detokenize{}.
+
+def _format_label_line(s: str, font: str = None) -> str:
     """
-    return rf'\detokenize{{{s}}}'
+    Wraps a string in LaTeX commands for styling, escaping special characters as needed.
+
+    Parameters:
+    - s: The input string (e.g., "group.path_name")
+    - font: The LaTeX font command to use (e.g., "textbf", "textsc")
+
+    Returns:
+    - A LaTeX-formatted string like \\texttt{\\textbf{a\\_b\\_c}}
+    """
+    # Escape LaTeX special characters that are commonly problematic in text mode
+    escape_map = {
+        # '\\': r'\\textbackslash{}',
+        '{': r'\{',
+        '}': r'\}',
+        '_': r'\_',
+        '&': r'\&',
+        '%': r'\%',
+        '$': r'\$',
+        '#': r'\#',
+        '~': r'\textasciitilde{}',
+        '^': r'\^{}',
+    }
+
+    escaped = ''.join(escape_map.get(char, char) for char in s)
+    if font is not None:
+        return rf"\{font}{{{escaped}}}"
+    else:
+        return escaped
+
 
 def _collect_connections(system):
     """
@@ -140,6 +103,7 @@ def _collect_connections(system):
             conns[source_sys, tgt_sys] = {source_var}
     return conns
 
+
 def _convert_varname_to_type(source_sys, var_name, abs2prom_out, var_names='local', ):
     if var_names in ('promoted', 'local'):
         name = abs2prom_out(f'{source_sys}.{var_name}')
@@ -151,8 +115,49 @@ def _convert_varname_to_type(source_sys, var_name, abs2prom_out, var_names='loca
     else:
         raise ValueError('var_names must be one of "absolute", "promoted", or "local".')
 
-def create_xdsm(problem_or_group, recurse=True, recurse_exceptions=None, use_full_path=False,
-                show_autoivc=False, var_map=None, var_names='local', include_nl_run_once=False):
+
+def _find_connection_system(pathname: str, systems: Sequence[str]) -> str:
+    """
+    Find the pathname in the known list of systems.
+
+    If the pathname does not exist, try removing the part after and including the final '.'
+    and search again. If we did not recurse into certain groups, then this will effectively
+    treat the group as the source or target of a given connection.
+
+    Parameters
+    ----------
+    pathname
+        The pathname that is either the source or target of a connection.
+    systems
+        The systems we've added to the XDSM.
+
+    Raises
+    ------
+    ValueError
+        If no compatible system is found.
+
+    Returns
+    -------
+    str
+        The pathname in the system that matches the given pathname.  This is either an identical string,
+        or the "most recent parent" system.
+    """
+    if pathname in systems.keys():
+        return pathname
+    elif '.' in pathname:
+        pathname, _, _ = pathname.rpartition('.')
+        return _find_connection_system(pathname, systems)
+    else:
+        raise ValueError(f'Could not find {pathname} in known systems.')
+
+
+def _parent_groups(pathname: str) -> list[str]:
+    parts = pathname.split('.')
+    return [''] + ['.'.join(parts[:i+1]) for i in range(len(parts))]
+
+
+def create_xdsm(problem_or_group, recurse=True, recurse_exceptions=None, use_full_path=True,
+                var_map=None, var_names='local', include_nl_run_once=False):
     """
     Create an XDSM PDF file using pyXDSM.
 
@@ -166,8 +171,6 @@ def create_xdsm(problem_or_group, recurse=True, recurse_exceptions=None, use_ful
         _description_, by default None
     use_full_path : bool, optional
         _description_, by default False
-    show_autoivc : bool, optional
-        _description_, by default False
     var_map : _type_, optional
         _description_, by default None
     var_names : str, optional
@@ -176,98 +179,168 @@ def create_xdsm(problem_or_group, recurse=True, recurse_exceptions=None, use_ful
     if XDSM is None:
         raise RuntimeError('create_xdsm requires the pyxdsm package. Try `pip install pyxdsm')
 
-    _var_map = var_map or {}
-
     if isinstance(problem_or_group, om.Problem):
         _model = problem_or_group.model
+        driver = problem_or_group.driver
     else:
         _model = problem_or_group
-
-    _top_pathname = _model.pathname if _model.pathname else 'Problem.model'
+        driver = None
 
     abs2prom_out = _model._resolver.abs2prom
 
     # Change `use_sfmath` to False to use computer modern
     xdsm = XDSM(use_sfmath=True)
-    sys_outputs = dict()
 
     conns = _collect_connections(_model)
+    indep_vars = list(_model.get_io_metadata(is_indep_var=True).keys())
+    des_vars = list(_model.get_io_metadata(is_design_var=True).keys())
 
-    solvers = deque()
+    solvers = []
+    solver_conns = {}
+    constraint_conns = {}
+    obj_conns = {}
+    systems = {}
+
+    if recurse_exceptions is None:
+        recurse_exceptions = []
+
+    if driver is not None:
+        xdsm.add_system('driver', OPT, label=_make_label(['Optimizer', driver.__class__.__name__], font='textbf'))
 
     for sys in _model.system_iter(include_self=False, recurse=recurse):
         kwargs = {'stack': False, 'faded': False, 'label_width': None, 'spec_name': None}
-        # depth = sys.pathname.count('.')
+
 
         # keyword arguments that are common regardless of system type.
         name = _make_legal_node_name(sys.pathname)
-        label = sys.pathname if use_full_path else sys.pathname.split('.')[-1]
-        label = _detokenize(label)
+        label = [sys.pathname]
+        if not use_full_path:
+            label = label.split('.')[-1:]
 
         if isinstance(sys, om.Group):
-            if recurse:
+            if not recurse:
+                recurse_exceptions.append(sys.pathname)
+            if sys.pathname not in recurse_exceptions:
                 kind = SOLVER
                 solver = sys.nonlinear_solver
-                name = _make_legal_node_name(f'{solver}_{sys.pathname}')
-                solvers.append(name)
-                label = [_detokenize(str(solver)), label]
+                if not include_nl_run_once and isinstance(solver, om.NonlinearRunOnce):
+                    continue
+                name = _make_legal_node_name(sys.pathname)
+                solvers.append(sys.pathname)
+                label = [str(solver)] + label
             else:
                 kind = GROUP
-                sys_outputs[name] = list(sys.list_outputs(out_stream=None, return_format='dict').keys())
 
         elif isinstance(sys, Component):
-            if sys.pathname == '_auto_ivc' and not show_autoivc:
+            if isinstance(sys, om.IndepVarComp):
+                # Don't show IVCs
                 continue
+
             kind = FUNC
 
             if isinstance(sys, ImplicitComponent):
                 imp_outputs = sys.list_outputs(residuals=True, out_stream=None, return_format='dict')
-                for imp_output in imp_outputs.keys():
-                    for solver_node_name in solvers:
-                        xdsm.connect(solver_node_name,
-                                     _make_legal_node_name(sys.pathname),
-                                     sorted({_detokenize(v) for v in imp_outputs.keys()}))
-                        xdsm.connect(_make_legal_node_name(sys.pathname),
-                                     solver_node_name,
-                                     sorted({rf'\mathcal{{R}}\left({_detokenize(v)}\right)' for v in imp_outputs.keys()}))
-            else:
-                sys_outputs[name] = list(sys.list_outputs(out_stream=None, return_format='dict').keys())
+                parent_groups = _parent_groups(sys.pathname)
+                for solver_pathname in solvers:
+                    if solver_pathname in parent_groups:
+                        solver_conns[solver_pathname, sys.pathname] = list(imp_outputs.keys()), False
+                        solver_conns[sys.pathname, solver_pathname] = list(imp_outputs.keys()), True
         else:
             raise RuntimeError('Unexpected system type', sys)
 
-        xdsm.add_system(name, kind, label=label, **kwargs)
-
+        for ex in recurse_exceptions:
+            if sys.pathname.startswith(f'{ex}.'):
+                break
+        else:
+            systems[sys.pathname] = kind
+            xdsm.add_system(name, kind, label=_make_label(label, font='textbf' if kind in (SOLVER,) else 'textsc'), **kwargs)
 
     #
-    # Render the connections
+    # Render the system connections
     #
     for (source_sys, tgt_sys), output_vars in conns.items():
-        if source_sys == '_auto_ivc' and not show_autoivc:
-            from_auto_ivc = set()
+        if isinstance(_model._get_subsystem(source_sys), om.IndepVarComp):
+            from_inputs = set()
+            from_driver = set()
             for var in output_vars:
                 var_label = _convert_varname_to_type(source_sys, var, abs2prom_out, var_names)
-                from_auto_ivc.add(var_label)
-            xdsm.add_input(_make_legal_node_name(tgt_sys), sorted({_detokenize(v) for v in from_auto_ivc}))
+                tgt_abs_path = f'{tgt_sys}.{var_label}'
+                if tgt_abs_path in des_vars:
+                    from_driver.add(var_label)
+                elif tgt_abs_path in indep_vars:
+                    from_inputs.add(var_label)
+            if from_inputs:
+                tgt_sys = _find_connection_system(tgt_sys, systems)
+                xdsm.add_input(_make_legal_node_name(tgt_sys), _make_label(sorted(from_inputs), font=None))
+            if from_driver:
+                tgt_sys = _find_connection_system(tgt_sys, systems)
+                xdsm.connect(_make_legal_node_name('driver'),
+                             _make_legal_node_name(tgt_sys),
+                             _make_label(sorted(from_driver), font=None))
         else:
-            if not recurse:
-                # If we're not recursing into groups, then connect the groups rather than
-                # their internal components.
-                source_sys = source_sys.split('.')[0]
-                tgt_sys = tgt_sys.split('.')[0]
+            source_sys = _find_connection_system(source_sys, systems)
+            tgt_sys = _find_connection_system(tgt_sys, systems)
+
+            src_idx = list(systems.keys()).index(source_sys)
+            tgt_idx = list(systems.keys()).index(tgt_sys)
+
             if source_sys != tgt_sys:
                 xdsm.connect(_make_legal_node_name(source_sys),
                             _make_legal_node_name(tgt_sys),
-                            sorted({_detokenize(v) for v in output_vars}))
+                            sorted({rf'\detokenize{{{v}}}' for v in output_vars}))
 
-    for sys_name, outputs in sys_outputs.items():
-        for (source_sys, tgt_sys), output_vars in conns.items():
-            if not recurse:
-                source_sys = source_sys.split('.')[0]
+                if src_idx > tgt_idx:
+                    # This is a feedback connection, and should include connections to the parent solver.
+                    parent_groups = _parent_groups(source_sys)
+                    for solver_pathname in solvers:
+                        # continue
+                        if solver_pathname in parent_groups:
+                            solver_conns[solver_pathname, tgt_sys] = sorted(output_vars), False
+                            solver_conns[source_sys, solver_pathname] = sorted(output_vars), True
 
+    #
+    # Render the solver connections
+    #
 
-    print(sys_outputs)
-    print(conns)
-    # elif outputs:
-    #     xdsm.add_output(name, sorted({_detokenize(v) for v in outputs.keys()}), side='right')
+    for (source_sys, tgt_sys), (output_vars, is_resid) in solver_conns.items():
+        source_sys = _find_connection_system(source_sys, systems)
+        tgt_sys = _find_connection_system(tgt_sys, systems)
+
+        if source_sys != tgt_sys:
+            if is_resid:
+                conn_vars = sorted({rf'\mathcal{{R}}(\detokenize{{{v}}})' for v in output_vars})
+            else:
+                conn_vars = sorted({rf'\detokenize{{{v}}}' for v in output_vars})
+            xdsm.connect(_make_legal_node_name(source_sys),
+                         _make_legal_node_name(tgt_sys),
+                         conn_vars)
+
+    #
+    # Render the Driver connections
+    #
+    if driver is not None:
+        for con_meta in driver._cons.values():
+            source_sys, _, output = con_meta['source'].rpartition('.')
+            source_sys = _find_connection_system(source_sys, systems)
+            if source_sys not in constraint_conns:
+                constraint_conns[source_sys] = [output]
+            else:
+                constraint_conns[source_sys].append(output)
+        for con_source, con_vars in constraint_conns.items():
+            xdsm.connect(_make_legal_node_name(con_source),
+                         _make_legal_node_name('driver'),
+                         [f'g({s})' for s in con_vars])
+
+        for obj_meta in driver._objs.values():
+            source_sys, _, output = obj_meta['source'].rpartition('.')
+            source_sys = _find_connection_system(source_sys, systems)
+            if source_sys not in obj_conns:
+                obj_conns[source_sys] = [output]
+            else:
+                obj_conns[source_sys].append(output)
+        for obj_source, obj_vars in obj_conns.items():
+            xdsm.connect(_make_legal_node_name(obj_source),
+                         _make_legal_node_name('driver'),
+                         [f'f({s})' for s in obj_vars])
 
     xdsm.write('xdsm')
